9edcb6c4c478c40df45a45ffc00c6b408c27eb4f
JVinay7 <118791692+JVinay7@users.noreply.github.com>
2023-06-06 16:14:35 +0530

    Update streamlit_app.py
    change symbols

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.header('Breakfast Favorites')
-streamlit.text('🥣 🥗 🐔 🥑🍞Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 kale,Spanich & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
