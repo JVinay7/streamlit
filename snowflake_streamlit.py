207ab0309e5ceeaaa8b8dd1b4e5a84ce74bfdc4a
JVinay7 <118791692+JVinay7@users.noreply.github.com>
2023-08-01 12:06:12 +0530

    Update snowflake_streamlit.py

--- snowflake_streamlit.py
+++ snowflake_streamlit.py
@@ -4,6 +4,14 @@ import plotly.express as px
 import requests
 import yfinance as yahooFinance
 from yahoo_fin import stock_info as si
+import snowflake.connector
 import warnings
 warnings.filterwarnings("ignore")
 st.set_page_config(layout="wide")
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
