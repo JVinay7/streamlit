6b6177e5d2c69337bb5c8f176a4f995626854210
JVinay7 <118791692+JVinay7@users.noreply.github.com>
2023-08-01 11:59:40 +0530

    Update snowflake_streamlit.py

--- snowflake_streamlit.py
+++ snowflake_streamlit.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot  as plt
 import plotly.express as px
 import requests
 import yfinance as yahooFinance
 from yahoo_fin import stock_info as si
+import warnings
+warnings.filterwarnings("ignore")
+st.set_page_config(layout="wide")
