78e1e331b4d9ed22eb54461bc3f9b7976ffc83aa
JVinay7 <118791692+JVinay7@users.noreply.github.com>
2023-08-01 14:36:09 +0530

    Update snowflake_streamlit.py

--- snowflake_streamlit.py
+++ snowflake_streamlit.py
@@ -7,10 +7,14 @@ from urllib.error import URLError
 warnings.filterwarnings("ignore")
 st.set_page_config(layout="wide")
 
+original_title1 = '<p style="font-family:Courier;text-align:center; color:Blue; font-size: 40px;">Investment Portfolio Dashboard</p>'
+st.write(original_title1 ,unsafe_allow_html=True)
+
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
+my_cur.execute("select * from my_share_profiletb")
+my_data_row = my_cur.fetchall()
+data = pd.read_csv(my_data_row)
 st.text("Hello from Snowflake:")
-st.text(my_data_row)
+st.dataframe(my_data_row)
 
