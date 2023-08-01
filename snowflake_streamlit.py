42801e98284d8df439462de8e83132caa62c1e7b
JVinay7 <118791692+JVinay7@users.noreply.github.com>
2023-08-01 11:52:11 +0530

    Create snowflake_streamlit.py

---
+++ snowflake_streamlit.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot  as plt
+import plotly.express as px
+import requests
+import yfinance as yahooFinance
+from yahoo_fin import stock_info as si
