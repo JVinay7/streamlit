445af6438e80a53b3c8cfcf5651b0e736b59199a
JVinay7 <118791692+JVinay7@users.noreply.github.com>
2023-08-02 10:31:01 +0530

    live update

--- snowflake_streamlit.py
+++ snowflake_streamlit.py
@@ -77,12 +77,88 @@ with col2:
     data5.plot.bar(alpha=0.8, ax=bar_ax, rot=45)
     st.pyplot(bar_fig)
 
-if(a1=="ETF"):
-    pass
-else:
-    st.sidebar.markdown("## SHARES :")
-    n2=list(data3["SHARES"].unique())
-    a2 = st.sidebar.selectbox("SHARES", n2)
-    data6=data3[data3["SHARES"]==a2]
-st.write(data6)
+# if(a1=="ETF"):
+#     pass
+# else:
+#     st.sidebar.markdown("## SHARES :")
+#     n2=list(data3["SHARES"].unique())
+#     a2 = st.sidebar.selectbox("SHARES", n2)
+#     data6=data3[data3["SHARES"]==a2]
+# st.write(data6)
+
+agree=st.sidebar.checkbox("Want to compare with live update")
+p=0
+if agree:
+    #data=pd.read_excel("my_share_profile.xlsx")
+    my_cur.execute("select * from my_share_profiletb")
+    my_data_row = my_cur.fetchall()
+    data = pd.DataFrame(my_data_row, columns=[desc[0] for desc in my_cur.description])
+    data=data[data["CATEGORY"]!="ETF"]
+    data["Live Price"]=data["tickerforyahoo"].apply(lambda i:round(si.get_live_price(i), 3))
+    data["Total Amount"]=data["Live Price"]*data["ShareCount"]
+    data["Total Profit"]=data["Total Amount"]-data["INVESTEDAMOUNT"]
+    data["Per Share perofit"]=data["Live Price"]-data["buyprice"]
+    df = data.to_csv("new_data.csv",index=False)
+    st.write(df.columns)
+    
+# data=pd.read_csv("new_data.csv")
+# Total_Invested_Amount=data["INVESTEDAMOUNT"].sum()
+# Total_current_value=data["Total Amount"].sum()
+# total_profit=Total_current_value-Total_Invested_Amount
+# original_title1 = '<p style="font-family:Courier;text-align:left; color:Blue; font-size: 30px;">Comparing With Return</p>'
+# st.write(original_title1  ,unsafe_allow_html=True)
+
+# col1, col2 ,col3= st.columns(3)
+# with col1:
+#     st.write("INVESTEDAMOUNT",Total_Invested_Amount)
+# with col2:
+#     st.write("Current Value",Total_current_value)
+# with col3:
+#     st.write("Profit",total_profit)
+# data1=data.groupby(["Category"]).agg({"INVESTEDAMOUNT":"sum","Total Amount":"sum","Total Profit":"sum"}).reset_index()
+# data1.columns=["Category","INVESTEDAMOUNT","Current Value","Total Profit"]
+# data1=data1.sort_values(by="Total Profit", axis=0, ascending=False)
+# st.table(data1)
+
+
+# if(a1=="ETF"):
+#     pass
+# else:
+#     data2=data[data["Category"]==a1]
+
+
+# original_title1 = '<p style="font-family:Courier;text-align:left; color:Blue; font-size: 30px;">Comparing with in Category</p>'
+# st.write(original_title1  ,unsafe_allow_html=True)
+# data3=data2[["shares","buy price","INVESTEDAMOUNT","Total Amount","Total Profit","Per Share perofit"]]
+# data3=data3.sort_values(by="Total Profit", axis=0, ascending=False)
+# st.table(data3)
+
+
+# original_title1 = '<p style="font-family:Courier;text-align:left; color:Blue; font-size: 30px;">Share wise Analysis</p>'
+# st.write(original_title1  ,unsafe_allow_html=True)
+
+# st.sidebar.markdown("## Shares :")
+# n2=list(data2["shares"].unique())
+# a2 = st.sidebar.selectbox("shares", n2)
+# data6=data2[data2["shares"]==a2]
+# data7=data6.drop(["index","Category","ticker for yahoo"],axis=1)
+# st.write(data7)
+# m=list(data6["ticker for yahoo"])
+# m1=m[0]
+
+# data_fin = yahooFinance.Ticker(m1)
+# data_fin1=data_fin.history(period="12mo")
+# data_fin2=data_fin1[["Close"]]
+# max1=list(data_fin2.max())
+# min1=list(data_fin2.min())
+# col1, col2 = st.columns(2)
+# with col1:
+#     st.write("Last 1 year Max Close Value",max1[0])
+# with col2:
+#     st.write("Last 1 year Min Close Value",min1[0])
+
+# original_title1 = '<p style="font-family:Courier;text-align:left; color:Blue; font-size: 30px;">Last 1 year closing plot</p>'
+# st.write(original_title1  ,unsafe_allow_html=True)
+
+# st.line_chart(data_fin2)
 
