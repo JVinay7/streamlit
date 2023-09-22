60baa6641960952702585facddef1f5ff0481ff4
JVinay7 <vinayjorige7@gmail.com>
2023-09-22 13:52:46 +0530

    second commit

--- button.py
+++ button.py
@@ -1,7 +1,24 @@
 import streamlit as st
 
-st.button("Reset", type="primary")
-if st.button('Say hello'):
-    st.write('Why hello there')
-else:
-    st.write('Goodbye')
+# st.button("Reset", type="primary")
+# if st.button('Say hello'):
+#     st.write('Why hello there')
+# else:
+#     st.write('Goodbye')
+
+# Create a Streamlit button
+if st.button("Open Pop-up"):
+    # When the button is clicked, set a flag to open the pop-up
+    open_popup = True
+
+# Check if the flag is set to open the pop-up
+if open_popup:
+    # Use st.modal to create the pop-up
+    with st.modal():
+        # Add content to the pop-up
+        st.write("This is a pop-up!")
+
+# Reset the flag when the pop-up is closed
+if not open_popup:
+    open_popup = False
+    
